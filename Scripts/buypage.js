e1c842cf62185fdeebe0618853755dabc6486bfb
Yash Mehrotra <yashmehrotra95@gmail.com>
2014-07-23 05:27:58 +0530

    Converted to Jquery

--- Scripts/buypage.js
+++ Scripts/buypage.js
@@ -2,44 +2,23 @@
 //Also need to add the whole jquery widget of autocomplete and also need to include jquery as link src in main file
 
 //
-//CHANGE THE JAVASCRIPT TO JQUERY
+//CHANGE THE JAVASCRIPT TO JQUERY   ---OH WAIT , YASH DID IT CAUSE HE IS AWESOME
 //
 
 
-// $(document).ready(function(){
+ $(document).ready(function(){
 	
-// 	// $("#seller-data").hide();
-// 	console.log("ready");
-// 	 $("#books-data").click(function(){
-// 	 	console.log("click");
-// 	// 	$("#seller-data").toggle();
-// 	 });
-// });
+	book_data_display();
+	click_toggle();
+
+});
 
 
 var booklist_autocomplete   = [];
 var authorlist_autocomplete = [];
 var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
 console.log(newURL);
-$.ajax({
-	type: "GET",
-	url: "/Books-for-bucks/sqldata.php",
-	success: function (result) {
-		if(result) {
-			var counter = 0;
-			var json = JSON.parse(result);
-			while(json[counter]) {
-				//booklist_autocomplete.push(/*Correct Response*/);
-				//authorlist_autocomplete.push(/*Correct Response*/);
-				console.log(json[counter].book_name + " " + json[counter].author_name);
-				$('#latest-outer > .latest-additions').append("<div class='books-data' id='books-data-"+json[counter].book_id+"' onClick='showthis(this.id)'>"+json[counter].book_name + " " + json[counter].author_name+"</div>");
-				$('#latest-outer > .latest-additions').append("<div class='seller-data' id='seller-data-"+json[counter].book_id+"' style='display:none;' >"+json[counter].seller_name + " " + json[counter].seller_phone+"</div>");
-				//console.log("2");
-				counter = counter +1;
-			}
-		}
-	}
-});
+
 
 function showthis(bookid) {
 	
@@ -47,5 +26,39 @@ function showthis(bookid) {
 	str = str.split("books-data-").join("");
 	console.log(str);
 	str = "#seller-data-" + str;
-	$(str).show();
+	$(str).toggle();
+}
+
+function book_data_display () {
+	
+	$.ajax({
+		type: "GET",
+		url: "/Books-for-bucks/sqldata.php",
+		success: function (result) {
+			if(result) {
+				var counter = 0;
+				var json = JSON.parse(result);
+				while(json[counter]) {
+					//booklist_autocomplete.push(/*Correct Response*/);
+					//authorlist_autocomplete.push(/*Correct Response*/);
+					console.log(json[counter].book_name + " " + json[counter].author_name);
+					$('#latest-outer > .latest-additions').append("<div class='books-data' id='books-data-"+json[counter].book_id+"' >"+json[counter].book_name + " " + json[counter].author_name+"</div>");
+					$('#latest-outer > .latest-additions').append("<div class='seller-data' id='seller-data-"+json[counter].book_id+"' style='display:none;'>"+json[counter].seller_name + " " + json[counter].seller_phone+"</div>");
+					//console.log("2");
+					counter = counter +1;
+				}
+			}
+		}
+	});
+}
+
+function click_toggle () {
+	
+	$('#latest-outer > .latest-additions').on('click', '.books-data', function() {
+		console.log("click");
+		var book_id = $(this).attr('id');
+		book_id = book_id.split("books-data-").join("");
+		var seller_data =  "#seller-data-" + book_id;
+		$(seller_data).toggle();
+	});
 }
