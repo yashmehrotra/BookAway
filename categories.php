dd7d85aad4cbb64343e9d5af70653c883d887a30
Yash Mehrotra <yashmehrotra95@gmail.com>
2014-10-08 01:42:11 +0530

    Added ascending in category sql query

--- categories.php
+++ categories.php
@@ -19,7 +19,7 @@
 
 	$buy_fixed = '<div class="sub-cbox"><label><input type="checkbox" class="sub-cbox-input" value="All" checked>All</label><br></div>';
 
-	$query = "SELECT * FROM tbl_categories";
+	$query = "SELECT * FROM tbl_categories ORDER BY category ASC ";
 	$category_data = mysqli_query($database_connection,$query);
 
 	if (strstr($current_url,$sell_url) || strstr($current_url,$edit_url)) {
