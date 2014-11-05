6db778e0a65455c76ae594cc6cf99ac96a041848
526avijitgupta <526avijitgupta@gmail.com>
2014-11-05 17:25:59 +0530

    yet to improve design of the whole page, added basic functionalities

--- about.php
+++ about.php
@@ -10,55 +10,57 @@
 		<script src="Scripts/top-panel.js"></script>
 	</head>
 	<body>
-		<?php
-		require_once('header.php');
-		?>
-		<div class="page-header">
-			<h2>About Us</h2>
-		</div>
+        <div class="outer-page-wrap">
+            <?php
+            require_once('header.php');
+            ?>
+            <div class="page-header">
+                <h2>About Us</h2>
+            </div>
 
-		<div class="main-containers" id="about-us-container">
-			<div class="developer-profile pointer-onhover" id="profile1">
-				<a href="Styles/Images/avijit.jpg"><img src="Styles/Images/avijit.jpg" class="profile-image" id="profile-image1" alt="img1"></a>
-				<p>Avijit Gupta
-					<br><br>
-					Frontend Developer
-				</p>
-				<div onclick="location.href='https://www.facebook.com/526avijitgupta';" class="pointer-onhover fb"></div>
-				<div onclick="location.href='http://www.twitter.com/526avijit';" class="pointer-onhover tw"></div>
-				<div onclick="location.href='https://www.linkedin.com/pub/avijit-gupta/9a/22/6';" class="pointer-onhover li"></div>
-				<div onclick="location.href='https://github.com/526avijitgupta';" class="pointer-onhover gi"></div>
-				<div onclick="location.href='';" class="pointer-onhover ml"></div>
-			</div>
+            <div class="main-containers" id="about-us-container">
+                <div class="developer-profile pointer-onhover" id="profile1">
+                    <a href="Styles/Images/avijit.jpg"><img src="Styles/Images/avijit.jpg" class="profile-image" id="profile-image1" alt="img1"></a>
+                    <p>Avijit Gupta
+                        <br><br>
+                        Frontend Developer
+                    </p>
+                    <div onclick="location.href='https://www.facebook.com/526avijitgupta';" class="pointer-onhover fb"></div>
+                    <div onclick="location.href='http://www.twitter.com/526avijit';" class="pointer-onhover tw"></div>
+                    <div onclick="location.href='https://www.linkedin.com/pub/avijit-gupta/9a/22/6';" class="pointer-onhover li"></div>
+                    <div onclick="location.href='https://github.com/526avijitgupta';" class="pointer-onhover gi"></div>
+                    <div onclick="location.href='';" class="pointer-onhover ml"></div>
+                </div>
 
-			<div class="developer-profile" id="profile2">
-				<a href="Styles/Images/yash.jpg"><img src="Styles/Images/yash.jpg" class="profile-image" id="profile-image2" alt="img2"></a>
-				<p>Yash Mehrotra
-					<br><br>
-					Backend Developer
-				</p>
-				<div onclick="location.href='https://www.facebook.com/yashm95';" class="pointer-onhover fb"></div>
-				<div onclick="location.href='http://www.twitter.com/yashm95';" class="pointer-onhover tw"></div>
-				<div onclick="location.href='in.linkedin.com/in/yashmehrotra';" class="pointer-onhover li"></div>
-				<div onclick="location.href='https://github.com/yashmehrotra/';" class="pointer-onhover gi"></div>
-				<div onclick="location.href='';" class="pointer-onhover ml"></div>
-			</div>
+                <div class="developer-profile" id="profile2">
+                    <a href="Styles/Images/yash.jpg"><img src="Styles/Images/yash.jpg" class="profile-image" id="profile-image2" alt="img2"></a>
+                    <p>Yash Mehrotra
+                        <br><br>
+                        Backend Developer
+                    </p>
+                    <div onclick="location.href='https://www.facebook.com/yashm95';" class="pointer-onhover fb"></div>
+                    <div onclick="location.href='http://www.twitter.com/yashm95';" class="pointer-onhover tw"></div>
+                    <div onclick="location.href='in.linkedin.com/in/yashmehrotra';" class="pointer-onhover li"></div>
+                    <div onclick="location.href='https://github.com/yashmehrotra/';" class="pointer-onhover gi"></div>
+                    <div onclick="location.href='';" class="pointer-onhover ml"></div>
+                </div>
 
-			<div class="developer-profile" id="profile3">
-				<a href="Styles/Images/sidhant.jpg"><img src="Styles/Images/sidhant.jpg" class="profile-image" id="profile-image3" alt="img3"></a>
-				<p>Sidhant Sharma
-					<br><br>
-					Graphics Designer
-				</p>
-				<div onclick="location.href='https://www.facebook.com/sid.sharma15';" class="pointer-onhover fb"></div>
-				<div onclick="location.href='http://www.twitter.com/SidMail15';" class="pointer-onhover tw"></div>
-				<div onclick="location.href='http://in.linkedin.com/pub/sidhant-sharma/91/960/230';" class="pointer-onhover li"></div>
-				<div onclick="location.href='https://github.com/sid15sharma';" class="pointer-onhover gi"></div>
-				<div onclick="location.href='';" class="pointer-onhover ml"></div>
-			</div>
-		</div>
-		<?php
-			require_once('footer.php');
-		?>
+                <div class="developer-profile" id="profile3">
+                    <a href="Styles/Images/sidhant.jpg"><img src="Styles/Images/sidhant.jpg" class="profile-image" id="profile-image3" alt="img3"></a>
+                    <p>Sidhant Sharma
+                        <br><br>
+                        Graphics Designer
+                    </p>
+                    <div onclick="location.href='https://www.facebook.com/sid.sharma15';" class="pointer-onhover fb"></div>
+                    <div onclick="location.href='http://www.twitter.com/SidMail15';" class="pointer-onhover tw"></div>
+                    <div onclick="location.href='http://in.linkedin.com/pub/sidhant-sharma/91/960/230';" class="pointer-onhover li"></div>
+                    <div onclick="location.href='https://github.com/sid15sharma';" class="pointer-onhover gi"></div>
+                    <div onclick="location.href='';" class="pointer-onhover ml"></div>
+                </div>
+            </div>
+            <?php
+                require_once('footer.php');
+            ?>
+        </div>
 	</body>
 </html>
--- header.php
+++ header.php
@@ -1,23 +1,27 @@
-<script type="text/javascript" src="Scripts/top-panel.js"></script>
+<!--<script type="text/javascript" src="Scripts/top-panel.js"></script>-->
 <script src="Scripts/scroll.js" type="text/javascript"></script>
 <noscript><meta http-equiv="refresh" content="0; url=nojs"></noscript>
 <!-- <link rel='stylesheet' media='screen and (min-width: 701px) and (max-width: 900px)' href='stylesheets/medium.css' /> -->
 <div class="header-wrapper">
+<!--<a href="index"><img src="Styles/Images/favicon1.png" class="pointer-onhover" id="favicon" alt="favicon" title="Home"></a>-->
+<img src="Styles/Images/favicon1.png" class="pointer-onhover" id="favicon" alt="favicon" title="Home">
+
 	<section class="header">
-		<a href="index"><img src="Styles/Images/favicon1.png" class="pointer-onhover" id="favicon" alt="favicon" title="Home"></a>
-		<ul>
-			<!-- <li><div class="top-divs"><a href="index" id="home">Home</a></div></li> -->
-			<li><a href="buy" id="buy">Buy</a></li>
-			<li><a href="sell" id="sell">Sell</a></li>
-			<li><a href="faqs" id="faqs">FAQs</a></li>
-			<li><a href="del" id="del">Edit</a></li>
-			<li><a href="feedback" id="feedback">Feedback</a></li>
-		</ul>
-		<a href="https://github.com/yashmehrotra/Bookaway" target="_blank">
-			<button id="fork-on-github" class="pointer-onhover">
-				Fork us on Github
-			</button>
-		</a>
+        <div class="header-menu">
+            <ul>
+                <li><a href="index" id="home">Home</a></li>
+                <li><a href="buy" id="buy">Buy</a></li>
+                <li><a href="sell" id="sell">Sell</a></li>
+                <li><a href="faqs" id="faqs">FAQs</a></li>
+                <li><a href="del" id="del">Edit</a></li>
+                <li><a href="feedback" id="feedback">Feedback</a></li>
+            </ul>
+            <a href="https://github.com/yashmehrotra/Bookaway" target="_blank">
+                <button id="fork-on-github" class="pointer-onhover">
+                    Fork us on Github
+                </button>
+            </a>
+        </div>
 	</section>
 </div>
 
