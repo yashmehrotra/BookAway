8468971291b8d52d595c9f1dda5cfe177d5f62f7
Yash Mehrotra <yashmehrotra95@gmail.com>
2014-11-11 23:07:19 +0530

    I am a genius

--- sqldata.php
+++ sqldata.php
@@ -9,6 +9,11 @@
 	}
 
 	$source = $_POST['source'];
+
+	$allowed_domains = 'bookaway.in' // Add proper domain
+	$reference_url = $_SERVER['HTTP_REFERER'];
+
+	//Check whether reference url is bookaway.in or localhost then only allow
 	
 	if($source == 'add_book') {
 
